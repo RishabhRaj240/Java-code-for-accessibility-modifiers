a63c29b88055856b79ec7dbd38ac2c98e1a813e2
Rishabh Raj <rishabhraj240405@gmail.com>
2024-05-02 00:02:56 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,15 +1,15 @@
 //Sample.java
 class Sample {
-	public static void main(String[] args) {
+   public static void main(String[] args) {
 
-		//System.out.println(a);  //CE: cannot find symbol...
-		//System.out.println(x);  //CE: cannot find symbol...
+	//System.out.println(a);  //CE: cannot find symbol...
+	//System.out.println(x);  //CE: cannot find symbol...
 
-		System.out.println(Example.a);
-		//System.out.println(Example.x);  //CE: NSV x cannot...
+	System.out.println(Example.a);
+	//System.out.println(Example.x);  //CE: NSV x cannot...
 
-		Example e2 = new Example();
-		System.out.println(e2.a);  //10
-		System.out.println(e2.x);  //20
-	}
+	Example e2 = new Example();
+	System.out.println(e2.a);  //10
+	System.out.println(e2.x);  //20
+   }
 }
