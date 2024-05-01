8693a7f7371d975219ac5fa4b786a4b877664afa
Rishabh Raj <rishabhraj240405@gmail.com>
2024-05-02 00:03:58 +0530

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,17 +1,17 @@
 //Example.java
 class Example {
-	static int a = 10;
-	int x = 20;
+   static int a = 10;
+   int x = 20;
 
-	public static void main(String[] args) {
-		System.out.println(a);
-		//System.out.println(x);  //CE: NSV X cannot....
+   public static void main(String[] args) {
+	System.out.println(a);
+	//System.out.println(x);  //CE: NSV X cannot....
 
-		System.out.println(Example.a);
-		//System.out.println(Example.x);  //CE: NSV x cannot...
+	System.out.println(Example.a);
+	//System.out.println(Example.x);  //CE: NSV x cannot...
 
-		Example e1 = new Example();
-		System.out.println(e1.a);
-		System.out.println(e1.x);
-	}
+	Example e1 = new Example();
+	System.out.println(e1.a);
+	System.out.println(e1.x);
+   }
 }
